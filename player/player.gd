a9daf16e7095bdce04c92fa0f0730892a6bb65ba
Nonojim <nolan.pederencino@gmail.com>
2026-03-01 21:52:47 +0100

    AttackState + animation

--- player/player.gd
+++ player/player.gd
@@ -3,16 +3,26 @@ extends CharacterBody2D
 const SPEED = 100.0
 var input_vector = Vector2.ZERO ## object scope
 @onready var animation_tree: AnimationTree = $AnimationTree ## shortcut for get_node("AnimationTree")
+@onready var playback = animation_tree.get("parameters/StateMachine/playback") as AnimationNodeStateMachinePlayback
 ##internal fn start by _
 func _physics_process(_delta: float) -> void: ## ici delta precede underscore pour (void)delta
-	input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	if input_vector != Vector2.ZERO:
-		var direction_vector := Vector2(input_vector.x, -input_vector.y)
-		update_blend_position(direction_vector)
+	var state = playback.get_current_node()
+	match state:
+		"MoveState":
+			input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+			if input_vector != Vector2.ZERO:
+				var direction_vector := Vector2(input_vector.x, -input_vector.y)
+				update_blend_position(direction_vector)
+			if Input.is_action_just_pressed("attack"):
+				playback.travel("AttackState")
 
-	velocity = input_vector * SPEED
-	move_and_slide()
+			velocity = input_vector * SPEED
+			move_and_slide()
+		"AttackState":
+			pass
+
 
 func update_blend_position(direction_vector: Vector2) -> void:
 	animation_tree.set("parameters/StateMachine/MoveState/RunState/blend_position", direction_vector)
 	animation_tree.set("parameters/StateMachine/MoveState/StandState/blend_position", direction_vector)
+	animation_tree.set("parameters/StateMachine/AttackState/blend_position", direction_vector)
